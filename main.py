72793a2f269302e25fbcae74365f3fb7b57294dc
Ryan <Ryan@localhost.com>
2025-12-06 10:14:50 +0700

    Attempt to implment a loop

--- main.py
+++ main.py
@@ -12,11 +12,21 @@ if __name__ == '__main__':
         "Hash": 2048
     })
 
-    # Set a position using FEN
-    stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    # Start FEN
+    start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
     # Print to unicode
-    board = chess.Board(stockfish.get_fen_position())
+    board = chess.Board(start_fen)
+
+    for i in range(2):
+
+        # Get best move from Stockfish
+        best_move = stockfish.get_best_move()
+        print(f"Best move: {best_move}")
+
+        # Apply the move to the board
+        board.push_uci(best_move)
+
     print(board.unicode())
 
     print()
